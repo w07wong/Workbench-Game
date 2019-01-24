7127bf92d730955c0728707cf105762369c19f21
William Wng <w07wong@yahoo.com>
2019-01-23 20:23:17 -0800

    Outlines

--- js/workbench.js
+++ js/workbench.js
@@ -19,7 +19,6 @@ function dragMoveListener(event) {
         // keep the dragged position in the data-x/data-y attributes
         x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
         y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
-
     // translate the element
     target.style.webkitTransform =
         target.style.transform =
@@ -57,12 +56,18 @@ interact('.dropzone').dropzone({
         event.relatedTarget.classList.remove('can-drop');
         event.relatedTarget.classList.remove('dropped');
         event.relatedTarget.textContent = 'Dragged out';
-        $(event.target).data('toolsContained').splice($(event.target).data('toolsContained').length - 1, 1);
+        // Delete tool from toolsContained
+        if ($(event.relatedTarget).attr('data-index') !== undefined) {
+            $(event.target).data('toolsContained').splice($(event.relatedTarget).attr('data-index'), 1);
+        }
     },
     ondrop: function (event) {
         event.relatedTarget.classList.add('dropped');
         event.relatedTarget.textContent = 'Dropped';
-        $(event.target).data('toolsContained').push(event.relatedTarget.id);
+        var toolName = event.relatedTarget.getAttribute('data-name');
+        $(event.target).data('toolsContained').push(toolName);
+        // Store tool index
+        event.relatedTarget.setAttribute('data-index', $(event.target).data('toolsContained').length - 1);
     },
     ondropdeactivate: function (event) {
         // remove active dropzone feedback
@@ -85,16 +90,18 @@ function timer(timerDisplay) {
 }
 
 function startTimer() {
+    drawTools();
+    addToolsStorage();
     document.getElementById('startButton').hidden = true;
     var timerDisplay = document.getElementById('timer');
     timerInterval = setInterval(function() {timer(timerDisplay)}, 1000);
 }
 
-$(document).ready(function() {
+function addToolsStorage() {
     $('.dropzone').each(function(i, obj) {
-        $(obj).data('toolsContained', []);
+        $(obj).data('toolsContained', new Array());
     });
-});
+}
 
 function saveGroupings() {
     $('.dropzone').each(function(i, obj) {
@@ -102,6 +109,48 @@ function saveGroupings() {
     }); 
     $('.draggable').each(function(i, obj) {
         // undefined if object is never clicked
-        console.log($(obj).attr('id') + ": " + $(obj).attr('data-x') + ', ' + $(obj).attr('data-y'));
+        console.log($(obj).attr('data-name') + ": " + $(obj).attr('data-x') + ', ' + $(obj).attr('data-y'));
     }); 
+}
+
+var images = {
+    "1": "angle-grinder",
+    "2": "battery",
+    "3": "circular-saw",
+    "4": "drill",
+    "5": "hammer-drill",
+    "6": "hammer",
+    "7": "hand-saw",
+    "8": "impact-driver",
+    "9": "jigsaw",
+    "10": "orbital-sander",
+    "11": "reciprocating-saw",
+    "12": "right-angle-drill",
+    "13": "side-cutter",
+    "14": "snips",
+    "15": "utility-knife",
+    "16": "wrench"
+};
+
+var numTools;
+var topZIndex;
+function drawTools() {
+    numTools = Math.floor(Math.random() * 20) + 20;
+    topZIndex = numTools + 1;
+    for (i = 0; i < numTools; i++) {
+        var tool = Math.floor(Math.random() * Object.keys(images).length) + 1;
+        var workbench = document.getElementById('workbench');
+
+        var d = document.createElement('div');
+        d.innerHTML = "<img data-name=\"" + images[tool] + "\" class=\"draggable\" src=\"images\\" + tool + ".png\" width=\"150\" height=\"150\">";
+        var element = d.firstChild;
+        element.addEventListener('click', moveToFront);
+
+        workbench.appendChild(element);
+        // add event listener
+    }
+}
+
+function moveToFront() {
+    $(this).css('z-index', topZIndex++);
 }
